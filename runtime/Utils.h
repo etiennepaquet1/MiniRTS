68b4bca6e3e91cfbc2a787d34b93b7d5baa47e49
etiennepaquet <paquetetienne@gmail.com>
2025-10-20 12:26:57 -0400

    utils

---
+++ runtime/Utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sched.h>
+#include <iostream>
+
+void pin_to_core(size_t core_id) {
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(core_id, &cpuset);
+
+    pthread_t current_thread = pthread_self();
+    int rc = pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+    if (rc != 0) {
+        std::cerr << "Error calling pthread_setaffinity_np: " << rc << "\n";
+    }
+}
