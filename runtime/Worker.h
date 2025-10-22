9ac4028bebf258d3635eb4c73a4a4bdb619bbc57
etiennepaquet <paquetetienne@gmail.com>
2025-10-21 20:19:13 -0400

    Add SPSC queue per worker for task submission

--- runtime/Worker.h
+++ runtime/Worker.h
@@ -10,31 +10,38 @@
 
 class Worker {
     using WSQ = WorkStealingQueue<Task>;
+    using SPSCQ = rigtorp::SPSCQueue<Task>;
 
     std::thread thread_;
     std::unique_ptr<WSQ> wsq_;
+    std::unique_ptr<SPSCQ> spscq_;
     std::shared_ptr<std::atomic<bool>> shutdown_requested_;
     int core_affinity_;
 
 public:
     Worker(int core_affinity, std::shared_ptr<std::atomic<bool>> stop_flag, size_t queue_capacity):
         wsq_(std::make_unique<WSQ>(queue_capacity)),
+        spscq_(std::make_unique<SPSCQ>(queue_capacity)),
         shutdown_requested_(stop_flag),
         core_affinity_(core_affinity) {}
 
     void run() {
         thread_ = std::thread([this] {
             pin_to_core(core_affinity_);
-            // std::osyncstream(std::cout) << "Thread " << std::this_thread::get_id()
-            //     << " pinned to core " << core_affinity_ << std::endl;
+            long local_counter {0};
             while (!shutdown_requested_->load(std::memory_order_relaxed)) {
-                // std::osyncstream(std::cout) << "Worker " << core_affinity_
-                //           << " on thread " << std::this_thread::get_id() << std::endl;
-                if (!wsq_->empty()) {
-                    std::optional<Task> t = wsq_->pop();
-                    if (t.has_value()) {
-                        t.value().func();
+                if (wsq_->empty()) {
+                    while (!spscq_->empty() && wsq_->size() != wsq_->capacity()) {
+                        wsq_->emplace(*spscq_->front());
+                        spscq_->pop();
                     }
+                    // if wsq_ still empty try stealing from other queues
+                }
+                std::optional<Task> t = wsq_->pop();
+                if (t.has_value()) {
+                    assert(t.value().func);
+                    t.value().func();
+                    ++local_counter;
                 }
             }
         });
@@ -45,7 +52,8 @@ public:
     }
 
     void enqueue(const Task& task) noexcept {
-        wsq_->emplace(task);
+        assert(task.func);
+        spscq_->push(task);
     }
 };
 
