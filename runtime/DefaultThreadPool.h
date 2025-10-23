b271402a12f7558263a81569d9dd2829a83ce1b4
etiennepaquet <paquetetienne@gmail.com>
2025-10-23 01:49:16 -0400

    Separate ThreadPool concept from default thread pool

---
+++ runtime/DefaultThreadPool.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <thread>
+#include <vector>
+#include <memory>
+#include <functional>
+
+#include "Constants.h"
+#include "Worker.h"
+#include "ThreadPool.h"
+
+namespace rts {
+
+    class DefaultThreadPool {
+
+        std::vector<Worker> workers_; // deleting running std::thread objects? ub
+        size_t num_threads_;
+        std::shared_ptr<std::atomic<bool>> stop_flag_;
+        int round_robin_;
+        size_t queue_capacity_;
+
+    public:
+        DefaultThreadPool(
+            size_t num_threads = std::thread::hardware_concurrency(),
+            size_t queue_capacity = kDefaultCapacity) noexcept :
+                num_threads_(num_threads),
+                stop_flag_(std::make_shared<std::atomic<bool> >(false)),
+                round_robin_(0),
+                queue_capacity_(queue_capacity) {}
+
+        ~DefaultThreadPool() noexcept {
+            stop_flag_->store(true);
+            for (auto& worker : workers_) {
+                worker.join();
+            }
+        }
+
+        void init() noexcept {
+            workers_.reserve(num_threads_);
+            for (int i = 0; i < num_threads_; i++) {
+                workers_.emplace_back(i, stop_flag_, queue_capacity_);
+                workers_.back().run();
+            }
+        }
+
+        void finalize() noexcept {
+            stop_flag_->store(true, std::memory_order_relaxed);
+            for (auto& worker : workers_) {
+                worker.join();
+            }
+        }
+
+        void enqueue(const Task& task) noexcept {
+            assert(task.func);
+            // round robin until one of the queues has space
+            while (!workers_[round_robin_].try_enqueue(task)) {
+                round_robin_++;
+                if (round_robin_ == num_threads_)
+                    round_robin_ = 0;
+            }
+            // Avoid picking same Worker multiple times in a row.
+            round_robin_++;
+            if (round_robin_ == num_threads_)
+                round_robin_ = 0;
+        }
+    };
+
+    static_assert(ThreadPool<DefaultThreadPool>);
+}
--- runtime/ThreadPool.h
+++ runtime/ThreadPool.h
@@ -1,22 +1,10 @@
 #pragma once
 
-#include <thread>
-#include <vector>
-#include <memory>
 #include <functional>
-
-#include "Worker.h"
+#include "Task.h"
 
 
 namespace rts {
-
-#ifdef __cpp_lib_hardware_interference_size
-    inline constexpr std::size_t kCacheLine = std::hardware_destructive_interference_size;
-#else
-    inline constexpr std::size_t kCacheLine = 64;
-#endif
-
-    inline constexpr size_t kDefaultCapacity = 1024;
 
     template <typename T>
     concept ThreadPool = requires(T t, size_t num_threads, size_t queue_capacity, const Task& task) {
@@ -25,61 +13,4 @@ namespace rts {
         { t.finalize() } noexcept;
         { t.enqueue(task) } noexcept;
     };
-
-    class DefaultThreadPool {
-        static constexpr size_t kDefaultThreadPoolCapacity = kDefaultCapacity;
-
-        std::vector<Worker> workers_; // deleting running std::thread objects? ub
-        size_t num_threads_;
-        std::shared_ptr<std::atomic<bool>> stop_flag_;
-        int round_robin_;
-        size_t queue_capacity_;
-
-    public:
-        DefaultThreadPool(
-            size_t num_threads = std::thread::hardware_concurrency(),
-            size_t queue_capacity = kDefaultThreadPoolCapacity) noexcept :
-                num_threads_(num_threads),
-                stop_flag_(std::make_shared<std::atomic<bool> >(false)),
-                round_robin_(0),
-                queue_capacity_(queue_capacity) {}
-
-        ~DefaultThreadPool() {
-            stop_flag_->store(true);
-            for (auto& worker : workers_) {
-                worker.join();
-            }
-        }
-
-        void init() noexcept {
-            workers_.reserve(num_threads_);
-            for (int i = 0; i < num_threads_; i++) {
-                workers_.emplace_back(i, stop_flag_, queue_capacity_);
-                workers_.back().run();
-            }
-        }
-
-        void finalize() noexcept {
-            stop_flag_->store(true, std::memory_order_relaxed);
-            for (auto& worker : workers_) {
-                worker.join();
-            }
-        }
-
-        void enqueue(const Task& task) noexcept {
-            assert(task.func);
-            // round robin until one of the queues has space
-            while (!workers_[round_robin_].try_enqueue(task)) {
-                round_robin_++;
-                if (round_robin_ == num_threads_)
-                    round_robin_ = 0;
-            }
-            // Avoid picking same Worker multiple times in a roq
-            round_robin_++;
-            if (round_robin_ == num_threads_)
-                round_robin_ = 0;
-        }
-    };
-
-    static_assert(ThreadPool<DefaultThreadPool>);
 }
