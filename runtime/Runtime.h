779100a92b8b9802e01f592b1321a51b7941b03b
etiennepaquet <paquetetienne@gmail.com>
2025-10-23 01:50:27 -0400

    add thread_local tsl_worker

--- runtime/Runtime.h
+++ runtime/Runtime.h
@@ -1,9 +1,24 @@
 #pragma once
 
-#include <future>
+#include <atomic>
+#include <cassert>
+#include <thread>
+
+#include "Task.h"
 #include "ThreadPool.h"
 
+
+
 namespace rts {
+
+    class Worker;
+    class DefaultThreadPool;
+
+    template <typename T>
+    class Promise;
+
+    template <typename T>
+    class Future;
 
     inline static std::atomic<bool> running{false};
     inline static void* active_thread_pool = nullptr;
@@ -12,8 +27,10 @@ namespace rts {
     inline static void (*enqueue_fn)(const Task&) = nullptr;
     inline static void (*finalize_fn)() = nullptr;
 
-    template <ThreadPool T = DefaultThreadPool>
-    bool initialize_runtime(int num_threads = std::thread::hardware_concurrency()) noexcept {
+    inline thread_local Worker* tls_worker = nullptr;
+
+    template <ThreadPool T> // TODO: try to find a way to make default thread pool parameter
+    bool initialize_runtime(unsigned num_threads = std::thread::hardware_concurrency()) noexcept {
         bool expected = false;
         if (running.compare_exchange_strong(expected, true,
                                             std::memory_order_release,
@@ -46,6 +63,34 @@ namespace rts {
 
     inline void finalize() noexcept {
         finalize_fn();
+    }
+
+    // TODO: make sure the child tasks are enqueued on same worker
+    template<typename F, typename... Args>
+    auto async(F&& f, Args&&... args) -> Future<std::invoke_result_t<F, Args...>> {
+        using T = std::invoke_result_t<F, Args...>;
+
+        Promise<T> p;
+        auto fut = p.get_future();
+
+        // Capture the promise by value (moved)
+        Task task = [func = std::forward<F>(f),
+                     args_tuple = std::make_tuple(std::forward<Args>(args)...),
+                     p = std::move(p)]() mutable {
+            try {
+                if constexpr (std::is_void_v<T>) {
+                    std::apply(func, std::move(args_tuple));
+                    p.set_value();
+                } else {
+                    T result = std::apply(func, std::move(args_tuple));
+                    p.set_value(std::move(result));
+                }
+            } catch (...) {
+                p.set_exception(std::current_exception());
+            }
+        };
+        enqueue(std::move(task));
+        return fut;
     }
 
 } // namespace rts
--- runtime/Worker.h
+++ runtime/Worker.h
@@ -3,67 +3,71 @@
 #include <syncstream>
 #include <thread>
 
+#include "Runtime.h"
 #include "Task.h"
 #include "WorkStealingQueue/include/WorkStealingQueue.h"
 #include "SPSCQueue/include/rigtorp/SPSCQueue.h"
 #include "Utils.h"
 
 
-class Worker {
-    using WSQ = WorkStealingQueue<Task>;
-    using SPSCQ = rigtorp::SPSCQueue<Task>;
+namespace rts {
+    class Worker {
+        using WSQ = WorkStealingQueue<Task>;
+        using SPSCQ = rigtorp::SPSCQueue<Task>;
 
-    std::thread thread_;
-    std::unique_ptr<WSQ> wsq_;
-    std::unique_ptr<SPSCQ> spscq_;
-    std::shared_ptr<std::atomic<bool>> shutdown_requested_;
-    int core_affinity_;
+        std::thread thread_;
+        std::unique_ptr<WSQ> wsq_;
+        std::unique_ptr<SPSCQ> spscq_;
+        std::shared_ptr<std::atomic<bool>> shutdown_requested_;
+        int core_affinity_;
 
-public:
-    Worker(int core_affinity, std::shared_ptr<std::atomic<bool>> stop_flag, size_t queue_capacity):
-        wsq_(std::make_unique<WSQ>(queue_capacity)),
-        spscq_(std::make_unique<SPSCQ>(queue_capacity)),
-        shutdown_requested_(stop_flag),
-        core_affinity_(core_affinity) {}
+    public:
+        Worker( int core_affinity,
+                const std::shared_ptr<std::atomic<bool>>& stop_flag,
+                size_t queue_capacity) noexcept :
+                    wsq_(std::make_unique<WSQ>(queue_capacity)),
+                    spscq_(std::make_unique<SPSCQ>(queue_capacity)),
+                    shutdown_requested_(stop_flag),
+                    core_affinity_(core_affinity) {}
 
-    void run() {
-        thread_ = std::thread([this] {
-            pin_to_core(core_affinity_);
-            long local_counter {0};
-            while (!shutdown_requested_->load(std::memory_order_relaxed)) {
-                if (wsq_->empty()) {
-                    while (!spscq_->empty() && wsq_->size() != wsq_->capacity()) {
-                        wsq_->emplace(*spscq_->front());
-                        spscq_->pop();
+        void run() noexcept {
+            thread_ = std::thread([this] {
+                pin_to_core(core_affinity_);
+                long local_counter {0};
+                while (!shutdown_requested_->load(std::memory_order_relaxed)) {
+                    if (wsq_->empty()) {
+                        while (!spscq_->empty() && wsq_->size() != wsq_->capacity()) {
+                            wsq_->emplace(*spscq_->front());
+                            spscq_->pop();
+                        }
+                        // if wsq_ still empty try stealing from other queues
                     }
-                    // if wsq_ still empty try stealing from other queues
+                    std::optional<Task> t = wsq_->pop();
+                    if (t.has_value()) {
+                        assert(t.value().func);
+                        t.value().func();
+                        ++local_counter;
+                    }
                 }
-                std::optional<Task> t = wsq_->pop();
-                if (t.has_value()) {
-                    assert(t.value().func);
-                    t.value().func();
-                    ++local_counter;
-                }
-            }
-            std::osyncstream(std::cout) << "[Exit]: Thread " << core_affinity_
-                << ", Local counter: " << local_counter <<  std::endl;
-        });
-    }
+                std::osyncstream(std::cout) << "[Exit]: Thread " << core_affinity_
+                    << ", Local counter: " << local_counter <<  std::endl;
+            });
+            tls_worker = this;
+        }
 
-    void join() {
-        if (thread_.joinable())
-            thread_.join();
-    }
+        void join() noexcept {
+            if (thread_.joinable())
+                thread_.join();
+        }
 
-    void enqueue(const Task& task) noexcept {
-        assert(task.func);
-        spscq_->push(task);
-    }
+        void enqueue(const Task& task) const noexcept {
+            assert(task.func);
+            spscq_->push(task);
+        }
 
-    bool try_enqueue(const Task& task) noexcept {
-        assert(task.func);
-        return spscq_->try_push(task);
-    }
-};
-
-
+        [[nodiscard]] bool try_enqueue(const Task& task) const noexcept {
+            assert(task.func);
+            return spscq_->try_push(task);
+        }
+    };
+}
