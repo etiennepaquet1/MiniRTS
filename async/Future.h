8bcd13b535b264d62278e417d26af20a43efcf72
etiennepaquet <paquetetienne@gmail.com>
2025-10-30 01:55:15 -0400

    cleanup

--- async/Future.h
+++ async/Future.h
@@ -154,7 +154,6 @@ namespace rts::async {
     template<typename F>
     Future<std::invoke_result_t<F>> Future<void>::then(F &&f) {
         assert(state_ && "then() called on invalid Future<void>");
-        assert(!state_.unique() && "then() called on orphaned Future (Promise destroyed)");
         using U = std::invoke_result_t<F>;
 
         Promise<U> p;
